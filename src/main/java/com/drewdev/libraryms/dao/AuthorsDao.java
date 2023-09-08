5e4da78a03bd14cdbc49fa2ebe8c167072148840
drewmaruli <201900190@my.apiu.edu>
2023-09-08 17:42:56 +0700

    Authors Dao Updated

--- src/main/java/com/drewdev/libraryms/dao/AuthorsDao.java
+++ src/main/java/com/drewdev/libraryms/dao/AuthorsDao.java
@@ -1,5 +1,33 @@
 package com.drewdev.libraryms.dao;
 
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import com.drewdev.libraryms.model.Authors;
+
 public class AuthorsDao {
 
+	@PersistenceContext
+	private EntityManager em;
+	
+	public List<Authors> getAll() {
+		final String sql = "SELECT "
+				+ " a "
+				+ "FROM "
+				+ " Authors a";
+		final List<Authors> authors = this.em.createQuery(sql, Authors.class).getResultList();
+		return authors;
+	}
+	
+	public Authors insert(Authors author) {
+		em.persist(author);
+		return author;
+	}
+	
+	public Authors getAuthorById(String id) {
+		final Authors author = this.em.find(Authors.class, id);
+		return author;
+	}
 }
