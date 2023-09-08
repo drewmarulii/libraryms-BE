8d442179513fe6c91d1489417f49a602b8331aee
drewmaruli <201900190@my.apiu.edu>
2023-09-08 16:55:10 +0700

    Adding Some Models IV

--- src/main/java/com/drewdev/libraryms/model/Users.java
+++ src/main/java/com/drewdev/libraryms/model/Users.java
@@ -1,5 +1,56 @@
 package com.drewdev.libraryms.model;
 
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "m_users")
 public class Users {
+
+	private String userFullname;
+	private String userEmail;
+	private String userPassword;
+	private Roles role;
+	private File file;
+
+	public String getUserFullname() {
+		return userFullname;
+	}
+
+	public void setUserFullname(String userFullname) {
+		this.userFullname = userFullname;
+	}
+
+	public String getUserEmail() {
+		return userEmail;
+	}
+
+	public void setUserEmail(String userEmail) {
+		this.userEmail = userEmail;
+	}
+
+	public String getUserPassword() {
+		return userPassword;
+	}
+
+	public void setUserPassword(String userPassword) {
+		this.userPassword = userPassword;
+	}
+
+	public Roles getRole() {
+		return role;
+	}
+
+	public void setRole(Roles role) {
+		this.role = role;
+	}
+
+	public File getFile() {
+		return file;
+	}
+
+	public void setFile(File file) {
+		this.file = file;
+	}
 
 }
