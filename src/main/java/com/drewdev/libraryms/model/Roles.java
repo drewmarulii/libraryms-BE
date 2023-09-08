43dd186b7e5f1fbd8e193e6652d4fafa6d65190a
drewmaruli <201900190@my.apiu.edu>
2023-09-08 16:59:08 +0700

    Adding Some Models V

--- src/main/java/com/drewdev/libraryms/model/Roles.java
+++ src/main/java/com/drewdev/libraryms/model/Roles.java
@@ -1,5 +1,29 @@
 package com.drewdev.libraryms.model;
 
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "m_roles")
 public class Roles {
+
+	private String roleCode;
+	private String roleName;
+
+	public String getRoleCode() {
+		return roleCode;
+	}
+
+	public void setRoleCode(String roleCode) {
+		this.roleCode = roleCode;
+	}
+
+	public String getRoleName() {
+		return roleName;
+	}
+
+	public void setRoleName(String roleName) {
+		this.roleName = roleName;
+	}
 
 }
